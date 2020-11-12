d9056216d42a9ad7219f23097482a4765e8155ca
Михаил Луканин <mlukanin@timepath.ru>
2020-11-12 11:08:50 +0500

    Version 0.1 Signed-off-by: Михаил Луканин <mlukanin@timepath.ru>

---
+++ Address.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressOrganization
+{
+    class Address
+    {
+        /// <summary>
+        /// Улица
+        /// </summary>
+        public string street;
+
+        /// <summary>
+        /// Номер дома
+        /// </summary>
+        public int houseNumber;
+
+        /// <summary>
+        /// Корпус
+        /// </summary>
+        public string housing;
+
+        /// <summary>
+        /// Населенный пункт
+        /// </summary>
+        public string settlement;
+
+        /// <summary>
+        /// Район
+        /// </summary>
+        public string district;
+
+        /// <summary>
+        /// Республика, Край
+        /// </summary>
+        public string republic;
+
+        /// <summary>
+        /// Страна
+        /// </summary>
+        public string country;
+
+        /// <summary>
+        /// Индекс
+        /// </summary>
+        public int indexAddress;
+
+        private string _secret;
+        
+        public string AddressBuild
+        {
+            get
+            {
+                return "ул. " + this.street + ", д." + this.houseNumber.ToString()
+                        + ", "
+                        + (this.housing != "" && this.housing != null && this.housing != "0" ? "корп." + this.housing + ", " : "")
+                        + this.settlement
+                        + ", "
+                        + this.indexAddress.ToString();
+            }
+        }
+        public Address()
+        {}
+        public Address(
+            string street, 
+            int houseNumber,
+            string housing,
+            string settlement,
+            string district,
+            string republic,
+            string country,
+            int indexAddress
+            )
+        {
+            this.street = street;
+            this.houseNumber = houseNumber;
+            this.housing = housing;
+            this.settlement = settlement;
+            this.district = district;
+            this.republic = republic;
+            this.country = country;
+            this.indexAddress = indexAddress;
+
+            this._secret = this.housing != "" && this.housing != null && this.housing != "0" ? "Secret":"";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressOrganization
+{
+    class Program
+    {
+
+        static List<Address> adresses = new List<Address>();
+
+        static bool isRandom = true;
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("Привет! Я твой помощник по нашим адресам.");
+            Console.WriteLine("Введи нужную цифру из вариантов диалога.");
+            Console.WriteLine("1. Посмотреть все адреса");
+            Console.WriteLine("2. Создать свой адрес");
+            if (isRandom)
+            {
+                Console.WriteLine("3. Заполни 3 адреса автоматически");
+            }
+            Console.Write("Введи цифру: ");
+
+            try { 
+                var key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        GetAllAdress();
+                        break;
+
+                    case 2:
+                        createAddress();
+                        break;
+
+                    case 3:
+                        if (isRandom)
+                        {
+                            threeAddressCreate();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Увы я такой команды не знаю, давай нажмем любую клавишу, вернемся и попробуем снова)))");
+                            Console.ReadKey();
+                            Start();
+                        }
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Увы я такой команды не знаю, давай нажмем любую клавишу, вернемся и попробуем снова)))");
+                        Console.ReadKey();
+                        Start();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Чето сломалось, но это же не пугает бравых солдат, верно? Нажмем любую клавишу, вернемся и попробуем снова?");
+                Console.ReadKey();
+                Start();
+            }
+             
+        }
+
+
+        static void createAddress()
+        {
+            Console.Clear();
+            Console.WriteLine("Отлично! Давай создам наш адрес, но помни, заполнять нужно с умом!");
+            Console.WriteLine("");
+            
+            try
+            {
+                Address newAddress = new Address();
+                Console.Write("Введи улицу: ");
+                var street =  Console.ReadLine();
+                newAddress.street = street;
+
+                Console.Write("А теперь номер дома: ");
+                var houseNumber = Convert.ToInt32(Console.ReadLine());
+                newAddress.houseNumber = houseNumber;
+
+                Console.Write("А теперь корпус, если есть: ");
+                var housing = Console.ReadLine();
+                newAddress.housing = housing;
+
+                Console.Write("Населенный пункт: ");
+                var settlement = Console.ReadLine();
+                newAddress.settlement = settlement;
+
+                Console.Write("Район: ");
+                var district = Console.ReadLine();
+                newAddress.district = district;
+
+                Console.Write("Край: ");
+                var republic = Console.ReadLine();
+                newAddress.republic = republic;
+
+                Console.Write("Страну: ");
+                var country = Console.ReadLine();
+                newAddress.country = country;
+
+                Console.Write("И индекс: ");
+                var indexAddress = Convert.ToInt32(Console.ReadLine());
+                newAddress.indexAddress = indexAddress;
+
+                adresses.Add(newAddress);
+                Console.WriteLine("Отлично, адрес создан!");
+                Console.ReadKey();
+                Start();
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Как-то ты неправильно вводил данные. Охх, придеться все заново, извини.");
+                Console.WriteLine("Вот что случилось: ");
+                Console.Write(ex.Message);
+                Console.ReadKey();
+                Start();
+            }
+        }
+
+        static void GetAllAdress()
+        {
+            Console.Clear();
+            if (adresses.Count > 0)
+            {
+                int integer = 1;
+                Console.WriteLine("Вот все адреса");
+                Console.WriteLine("");
+                foreach (var address in adresses)
+                {
+                    Console.WriteLine("(" + integer.ToString() + ") " + address.AddressBuild
+                        );
+                    integer++;
+                }
+                Console.ReadKey();
+                Start();
+            }
+            else
+            {
+                Console.WriteLine("Увы, на данный момент адресов в системе нет. Нажми любую клавишу, чтобы вернуться к спискам выбора");
+                Console.ReadKey();
+                Start();
+            }
+        }
+
+        static void threeAddressCreate()
+        {
+            Console.Clear();
+            Address newAddress = new Address(
+                "Попова",
+                23,
+                "",
+                "Пермь",
+                "Ленинский",
+                "Пермский",
+                "Россия",
+                603320
+                );
+            adresses.Add(newAddress);
+
+            newAddress = new Address(
+                "Стахановская",
+                32,
+                "2",
+                "Пермь",
+                "Индустриальный",
+                "Пермский",
+                "Россия",
+                604320
+                );
+            adresses.Add(newAddress);
+
+            newAddress = new Address(
+                "Куликова",
+                23,
+                "",
+                "Москва",
+                "Ленинский",
+                "Пермский",
+                "Россия",
+                403420
+                );
+            adresses.Add(newAddress);
+
+            isRandom = false;
+            Console.WriteLine("Отлично, адреса созданы!");
+            Console.ReadKey();
+            Start();
+        }
+    }
+}
